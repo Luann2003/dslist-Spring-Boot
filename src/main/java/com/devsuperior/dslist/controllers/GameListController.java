61715a0be14265a12d3978dfd6dcac7f742aac3d
Luann2003 <lvictorluciano@outlook.com>
2023-07-14 20:08:06 -0300

    finalizado

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslist.dto.GameListDto;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.dto.ReplacementDto;
+import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+	@Autowired
+	private GameListService gameListService;
+	
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameListDto> findAll(){
+		List<GameListDto> result = gameListService.findAll();
+		return result;
+	}
+	
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByList(@PathVariable Long listId){
+		List<GameMinDTO> result = gameService.findByList(listId);
+		return result;
+	}
+	
+	@PostMapping(value = "/{listId}/replacement")
+	public void move(@PathVariable Long listId,@RequestBody ReplacementDto body){
+		gameListService.mov(listId, body.getSourceIndex(), body.getDestinationIndex());
+	}
+}
---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,37 @@
+package com.devsuperior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dslist.dto.GameDto;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
+import com.devsuperior.dslist.repositories.GameRepository;
+
+@Service
+public class GameService  {
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findAll(){
+		 List<Game> result = gameRepository.findAll();
+		 return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDto findById(Long id){
+		Game result = gameRepository.findById(id).get();
+		return new GameDto(result);
+	}
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		 List<GameMinProjection> result = gameRepository.searchByList(listId);
+		 return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+}
